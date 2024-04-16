c93da27a290ad8eb073e6e47116dc5f211995a55
mariakaraush <167077657+mariakaraush@users.noreply.github.com>
2024-04-16 11:03:55 +0000

    .

--- loops.js
+++ loops.js
@@ -1,8 +1,15 @@
-let value = 0;
-do {
-    value = value + 1;
-    console.log("Doing something useful")
-    console.log(value)
-} while (value < 10);
+// let value = 0;
+// do {
+//     value = value + 1;
+//     console.log("Doing something useful")
+//     console.log(value)
+// } while (value < 10);
 
-console.log("Just finished looping");
+// console.log("Just finished looping");
+
+const readline = require('readline-sync');
+let userValue = readline.question("Provide a value: ");
+
+for ( let i = 1; i <= 12; i++) {
+    console.log(`${i} x ${userValue} = ${i * userValue}`)
+}
